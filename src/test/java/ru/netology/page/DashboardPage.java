90e8fc4360e74f3d6ca88afc4009cfe3822a69e0
Valeriya <tmcxezsim5nn@mail.ru>
2022-01-27 14:56:27 +0300

    main page

---
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -0,0 +1,30 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+    private SelenideElement heading = $(withText("Путешествие дня"));
+    private SelenideElement buyButton = $$(".button_theme_alfa-on-white .button__text").findBy(exactText("Купить"));
+    private SelenideElement creditButton = $$(".button_theme_alfa-on-white .button__text").findBy(exactText("Купить в кредит"));
+
+    public DashboardPage() {
+        heading.shouldBe(visible);
+        buyButton.shouldBe(visible);
+        creditButton.shouldBe(visible);
+    }
+
+    public BuyPage buyPage() {
+        buyButton.click();
+        return new BuyPage();
+    }
+
+    public CreditPage creditPage() {
+        creditButton.click();
+        return new CreditPage();
+    }
+}
