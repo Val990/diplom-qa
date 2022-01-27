93d51f403d53940ae0195bf4c52ad8dd2be9343c
Valeriya <tmcxezsim5nn@mail.ru>
2022-01-27 14:55:23 +0300

    page added

---
+++ src/test/java/ru/netology/page/BuyPage.java
@@ -0,0 +1,50 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.CardInfo;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class BuyPage {
+    private SelenideElement heading = $$("h3").find(Condition.text("Оплата по карте"));
+    private SelenideElement cardNumber = $("[placeholder='0000 0000 0000 0000']");
+    private SelenideElement month = $("[placeholder='08']");
+    private SelenideElement year = $("[placeholder='22']");
+    private SelenideElement client = $(byText("Владелец")).parent().$(".input__control");
+    private SelenideElement cvc = $("[placeholder='999']");
+    private SelenideElement continueButton = $$(".button_theme_alfa-on-white .button__text").findBy(exactText("Продолжить"));
+    private SelenideElement success = $(".icon_name_ok");
+    private SelenideElement error = $(".icon_name_error");
+    private SelenideElement invalidData = $(".input__sub");
+
+    public BuyPage() {
+        heading.shouldBe(visible);
+    }
+
+    public void fillForm(CardInfo cardInfo) {
+        cardNumber.setValue(cardInfo.getNumber());
+        month.setValue(cardInfo.getMonth());
+        year.setValue(cardInfo.getYear());
+        client.setValue(cardInfo.getClient());
+        cvc.setValue(cardInfo.getCvc());
+        continueButton.click();
+    }
+
+    public void successStatus() {
+        success.shouldBe(visible, Duration.ofSeconds(15));
+    }
+
+    public void errorStatus() {
+        error.shouldBe(visible, Duration.ofSeconds(15));
+    }
+
+    public String invalidData() {
+        return invalidData.getText();
+    }
+}
